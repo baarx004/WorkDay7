38f47007ca78b042dfc4b879f308789a179f85a6
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:51 -0500

    Work Day 7 Completed.

--- DoWhileDemo/DoWhileDemo/Module1.vb
+++ DoWhileDemo/DoWhileDemo/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
     Sub Main()
         Dim num As Integer = 1
 
-        Do While num <= 7
+        Do While num = 10
             Console.WriteLine(" " & num)
             num = num + 1
         Loop
--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,18 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
+        Dim population As Double
+        Dim year As Integer
+        population = 7000.0
+        year = 2014
+        Do While (population > 6)
+            population = population / 2
+            year = year - 50
+        Loop
+
+
+        MessageBox.Show("The answer is " & year)
 
     End Sub
+
 End Class
